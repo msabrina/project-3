e8e310647f3a3795f45c66327ab24651b89dd136
Joey Pinhas <jpinhas97@yahoo.com>
2016-11-22 21:53:55 -0500

    Gets user auth working with JWT; basic functionality

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,19 @@
-const router = require('express').Router();
+const router    = require('express').Router();
 const userModel = require('../models/user.js');
+const auth      = require('../lib/auth.js');
 
 function sendAsJSON (req, res, next) {
-  // res.json({message: 'works'});
   res.json(res.rows);
 }
+
+router.route('/users/:id/login')
+  .get(userModel.logIn, sendAsJSON)
+
+router.route('/users/:token')
+  .get(userModel.getUserData, sendAsJSON)
+  .delete(sendAsJSON);
+
 router.route('/users')
-  .get(sendAsJSON)
   .post(userModel.createUser, sendAsJSON);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 'use strict';
 require('dotenv').config({ silent: true });
-const express    = require('express');
-const logger     = require('morgan');
-const path       = require('path');
-const app        = express();
-const PORT       = process.argv[2] || process.env.port || 3000;
-const bodyParser = require('body-parser');
+const express      = require('express');
+const logger       = require('morgan');
+const path         = require('path');
+const bodyParser   = require('body-parser');
+
+const app          = express();
+const PORT         = process.argv[2] || process.env.port || 3000;
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
