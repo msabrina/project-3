eb1c8e8fed05b420eb085537d301f49c4b120ae5
Joey Pinhas <jpinhas97@yahoo.com>
2016-11-25 10:51:43 -0500

    Updates db to include tables for unapproved users, along with survey questions, and adds a route and model for the survey questions

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,10 @@
-const router         = require('express').Router();
-const usersRouter    = require('./users.js');
-const productsRouter = require('./products.js');
+const router          = require('express').Router();
+const usersRouter     = require('./users.js');
+const productsRouter  = require('./products.js');
+const questionsRouter = require('./questions.js');
 
 router.use('/users', usersRouter);
 router.use('/products', productsRouter);
+router.use('/questions', questionsRouter);
 
 module.exports = router;
