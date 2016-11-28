4a26461c6e28b3f0212396441db0ed6dac81e596
Aaron Conklin <conklin.aaron@gmail.com>
2016-11-28 18:35:16 -0500

    Seeds

--- src/components/App/App1/Profile/MyPostContainer/MyPost/MyPost.jsx
+++ src/components/App/App1/Profile/MyPostContainer/MyPost/MyPost.jsx
@@ -4,11 +4,12 @@ import styles from './MyPost.css';
 const MyPost = props => (
   <div className={styles["my-post"]}>
     <div className={styles['post-text']}>
-      <p>Post Title</p>
-      <p>Post Description</p>
+      <p>Title</p>
+      <p>Price</p>
+      <p>Description</p>
     </div>
     <div className={styles['post-img-del']}>
-      <img src="" alt="Product Image"/>
+      <img src="/chairShare.png" alt="Product Image"/>
       <button>Delete</button>
     </div>
   </div>
