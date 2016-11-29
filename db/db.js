65ae317d9ea852d0144b3ac588371fbed30873a8
Joey Pinhas <jpinhas97@yahoo.com>
2016-11-29 18:46:41 -0500

    heroku stuff

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const pg = require('pg-promise')({});
 
 const pgConfig = {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
 };
 
 const db = pg(pgConfig);
