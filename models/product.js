87b3578b8de5dfd4c6906038e30f957b0b19ad26
Joey Pinhas <jpinhas97@yahoo.com>
2016-11-29 19:28:19 -0500

    Fixes the fix that I thought would fix the previous fix that didn't do a fix

--- models/product.js
+++ models/product.js
@@ -1,4 +1,4 @@
-const db         = require('../lib/db.js');
+const db         = require('../lib/dbConnect.js');
 const auth       = require('../lib/auth.js');
 const CloudinaryService = require('../services/cloudinary.js');
 
